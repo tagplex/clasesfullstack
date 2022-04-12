167fd2ac7e92c51651ea635d3d137c47b82b4b61
tagplex <kevinp274@gmail.com>
2022-04-12 19:51:05 -0400

    Agrego server en app.js y agrego carpeta clase 10

--- Fullstack/Modulo4/express/app.js
+++ Fullstack/Modulo4/express/app.js
@@ -4,7 +4,7 @@ let app = express();
 
 app.listen(3000, () => console.log("Servidor corriendo"));
 
-app.get("/contacto", function(req, res){
-    res.send("Bienvenidos al servidor de express, seccion de contacto")
+app.get("/", function(req, res){
+    res.send("Bienvenidos al servidor de express")
 })
 
