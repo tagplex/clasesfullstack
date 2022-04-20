17c1a0f79517362e82ab373b7910fc59cdeaec4e
tagplex <kevinp274@gmail.com>
2022-04-19 22:25:50 -0400

    Ingreso archivos a carpeta integrador

---
+++ Fullstack/integrador/mercadoliebre/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static("public"))
+
+app.listen(3000, (req, res) => {
+    console.log("Servidor corriendo en http://localhost:3000");
+});
+
+app.get("/", function(req, res){
+    res.sendFile(path.resolve(__dirname,"./views/home.html"))
+})
