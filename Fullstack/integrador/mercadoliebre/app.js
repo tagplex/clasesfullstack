ee77959094533ef06e866ee89677b2270c614cd1
tagplex <kevinp274@gmail.com>
2022-05-12 20:36:53 -0400

    Proyecto finalizado para ejecutar en local

--- Fullstack/integrador/mercadoliebre/app.js
+++ Fullstack/integrador/mercadoliebre/app.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const path = require("path");
-
 const app = express();
 
 app.use(express.static("public"))
 
 app.listen(3000, (req, res) => {
-    console.log("Servidor corriendo en http://localhost:3000");
+    console.log("Servidor corriendo");
 });
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Servidor funcionando");
+})
 
 app.get("/", function(req, res){
     res.sendFile(path.resolve(__dirname,"./views/home.html"))
