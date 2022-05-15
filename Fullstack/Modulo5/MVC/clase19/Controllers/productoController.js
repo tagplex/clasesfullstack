c480d6c48e8388768063f4b84846570e19f7533b
tagplex <kevinp274@gmail.com>
2022-05-15 19:13:18 -0400

    Agrego controladores de ejemplo, clase 19

--- Fullstack/Modulo5/MVC/clase19/Controllers/productoController.js
+++
@@ -1,4 +0,0 @@
-const controlador = {
-
-};
-modulo.exports = controlador;
---
+++ Fullstack/Modulo5/MVC/clase19/controllers/productoController.js
@@ -0,0 +1,10 @@
+let  productosController = {
+
+    listado: function(){
+        return "Estas listando productos";
+    },
+    crear: function(){},
+    detalle: function(){},
+    detalleComentarios: function(){}
+};
+modulo.exports = productosController;
